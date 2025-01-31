63ed3ef94e6e8f5a57e21a34af10082288a4e92d
lhs2121 <mast1207@naver.com>
2025-01-31 14:06:38 +0900

    몬스터->플레이어 감지후 팔로잉

--- PunkVenture/Assets/Script/MonsterMove.cs
+++ PunkVenture/Assets/Script/MonsterMove.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class MonsterMove : MonoBehaviour
 {
     [SerializeField]
+    Transform m_target;
+    [SerializeField]
     Rigidbody2D m_rigid;
     float m_coolTime = 3.0f;
     float m_reset = 3.0f;
@@ -12,14 +14,56 @@ public class MonsterMove : MonoBehaviour
     float m_speed = 30.0f;
     int m_dir = 1;
 
+    bool m_detected = false;
+
+    
     // Start is called before the first frame update
     void Start()
     {
-        m_rigid = GetComponent<Rigidbody2D>();
+        m_rigid = GetComponentInParent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        if(m_detected)
+        {
+            FollowPlayer();
+        }
+        else
+        {
+            FollowNothing();
+        }
+
+        m_rigid.velocity = new Vector2(m_dir * m_speed, m_rigid.velocity.y);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            m_target = collision.transform;
+            m_detected = true;
+        }
+    }
+
+    void FollowPlayer()
+    {
+        float targetDir = m_target.position.x - transform.position.x;
+        if(targetDir < 0.0f)
+        {
+            m_dir = -1;
+        }
+        else if(targetDir == 0.0f)
+        {
+            m_dir = 0;
+        }
+        else
+        {
+            m_dir = 1;
+        }
+    }
+    void FollowNothing()
     {
         m_coolTime -= Time.deltaTime;
         if (m_coolTime < 0.0f)
@@ -27,6 +71,5 @@ public class MonsterMove : MonoBehaviour
             m_coolTime = m_reset;
             m_dir = Random.Range(-1, 1);
         }
-        m_rigid.velocity = new Vector2(m_dir * m_speed, m_rigid.velocity.y);
     }
 }
