69e8ebed892581e4efa39d163629a977cddb8709
lhs2121 <mast1207@naver.com>
2025-02-07 12:40:22 +0900

    무기시스템

---
+++ PunkVenture/Assets/Script/Weopon/Staff.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Staff : MonoBehaviour, IWeopon
+{
+    public void Equip()
+    {
+    }
+
+    public void Attack(float level)
+    {
+    }
+}
---
+++ PunkVenture/Assets/Script/Weopon/Sword.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour, IWeopon
+{
+    public void Equip()
+    {
+    }
+    public void Attack(float level)
+    {
+        //안에서 칼애니메이션 재생, 공격 콜라이더 등등 구현
+    }
+}
